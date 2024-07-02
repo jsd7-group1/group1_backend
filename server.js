f5b30040d6537c77d48d40dfbc593291874c81ad
noeysoftware <noeysoftware@gmail.com>
2024-07-02 15:52:13 +0700

    deleted server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 5000;
