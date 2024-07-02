421a4a780d4bdd5eb40cadfb7c54018285db5f1e
noeysoftware <noeysoftware@gmail.com>
2024-07-02 15:50:12 +0700

    Set up the main server file.

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 5000;
