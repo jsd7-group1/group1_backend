92aef803601b0bb716f7be2a254ad94c3809793d
patchanaab <165882679+patchanaab@users.noreply.github.com>
2024-07-04 13:46:16 +0700

    setup

--- models/product.model.js
+++ models/product.model.js
@@ -2,5 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-    productId: { type: String}
+    productId: { type: String, required: true},
+    productName: { type: String, required: true},
+    description: { type: String },
+    price: { type: Number, required: true},
+    categoryId: { type: Number, required: true},
+    imageUrl: { type: String },
+    costPrice: { type: String, required: true},
+    salePrice: { type: String, required: true},
 })
