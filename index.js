944afb0a2b6f162854ecbc1f1ca306cafc3df4ec
Kanya.wd <117735915+kanyawadee24@users.noreply.github.com>
2024-07-05 09:35:24 +0700

    update index.js

--- index.js
+++ index.js
@@ -77,9 +77,51 @@ app.get("/", (req, res) => {
 });
 
 // API EndPoint
-app.post("/register", async (req, res) => {});
-app.post("/login", async (req, res) => {});
-app.get("/users", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  console.log(req.body.email);
+  if (!req.body.email)
+    return res.status(400).json({ message: "email is required" });
+  if (!req.body.email.includes("@"))
+    return res.status(400).json({ message: "invalid email" });
+  if (!req.body.fullName)
+    return res.status(400).json({ meesage: "fullName is required" });
+  if (!req.body.password || !req.body.confirmPassword)
+    return res
+      .status(400)
+      .json({ message: "password or confirmPassword is required" });
+  if (req.body.password !== req.body.confirmPassword)
+    return res.status(400).json({ message: "confirmPassword Not Match" });
+  if (!req.body.imageUrl)
+    return res.status(400).json({ message: "ImageUrl is required" });
+
+  // res.send('Register Success')
+  res.json({
+    message: "register success",
+    data: req.body,
+  });
+});
+
+app.post("/login", async (req, res) => {
+  console.log(req.body.email);
+  if (!req.body.email)
+    return res.status(400).json({ message: "email is required" });
+  if (!req.body.email.includes("@"))
+    return res.status(400).json({ message: "invalid email" });
+  if (!req.body.password)
+    return res.status(400).json({ message: "password is required" });
+
+  // res.send('login Success')
+  res.json({
+    message: "login success",
+    data: req.body,
+  });
+});
+
+// app.get("/users", async (req, res) => {
+//   console.log(req.body);
+//   // res.json('hii')
+// });
+
 app.get("/products", authenticateToken, async (req, res) => {
   const { user } = req.user;
   try {
