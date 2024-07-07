3090433190a6bacc634411177558966d1dbba413
Kanya.wd <117735915+kanyawadee24@users.noreply.github.com>
2024-07-08 02:39:38 +0700

    move to controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,26 +1,51 @@
 import user from "../../models/user.model.js";
 import NotFoundError from "../error/NotFoundError.js";
 
-//########## API - 1 Get All Users //##########
-const getAllUsers = async (req, res, next) => {
-  try {
-    const users = await user.getUsers();
-    res.status(200).json({ message: "Get All Users", data: users });
-  } catch (error) {
-    next(error);
-  }
+// Register Contrller
+const registerController = async (req, res) => {
+  console.log(req.body.email);
+  if (!req.body.email)
+    return res.status(400).json({ message: "email is required" });
+  if (!req.body.email.includes("@"))
+    return res.status(400).json({ message: "invalid email" });
+  if (!req.body.fullName)
+    return res.status(400).json({ meesage: "fullName is required" });
+  if (!req.body.password || !req.body.confirmPassword)
+    return res
+      .status(400)
+      .json({ message: "password or confirmPassword is required" });
+  if (req.body.password !== req.body.confirmPassword)
+    return res.status(400).json({ message: "confirmPassword Not Match" });
+  if (!req.body.imageUrl)
+    return res.status(400).json({ message: "ImageUrl is required" });
+
+  // res.send('Register Success')
+  res.json({
+    message: "register success",
+    data: req.body,
+  });
 };
-//########## API - 2 Get User By ID //##########
-const getUserById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const user = await user.getUserById(Number(id));
-    if (!user) {
-      throw new NotFoundError(`User with id ${id} not found`);
-    }
-    res.status(200).json({ message: "Get User By ID", data: user });
-  } catch (error) {
-    next(error);
-  }
+
+// Login
+const loginController = async (req, res) => {
+  console.log(req.body.email);
+  if (!req.body.email)
+    return res.status(400).json({ message: "email is required" });
+  if (!req.body.email.includes("@"))
+    return res.status(400).json({ message: "invalid email" });
+  if (!req.body.password)
+    return res.status(400).json({ message: "password is required" });
+
+  // res.send('login Success')
+  res.json({
+    message: "login success",
+    data: req.body,
+  });
 };
-//########## API - 3 POST LOGIN //##########
+
+// GetUser
+const getUserController = async (req, res, next) => {
+  res.send("Hi");
+};
+
+export { registerController, loginController, getUserController };
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,48 +1,10 @@
 import express from "express";
+import * as UserController from "../controllers/usersController.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  res.send("Hi");
-});
-router.post("/login", async (req, res) => {
-  console.log(req.body.email);
-  if (!req.body.email)
-    return res.status(400).json({ message: "email is required" });
-  if (!req.body.email.includes("@"))
-    return res.status(400).json({ message: "invalid email" });
-  if (!req.body.password)
-    return res.status(400).json({ message: "password is required" });
-
-  // res.send('login Success')
-  res.json({
-    message: "login success",
-    data: req.body,
-  });
-});
-
-router.post("/register", async (req, res) => {
-  console.log(req.body.email);
-  if (!req.body.email)
-    return res.status(400).json({ message: "email is required" });
-  if (!req.body.email.includes("@"))
-    return res.status(400).json({ message: "invalid email" });
-  if (!req.body.fullName)
-    return res.status(400).json({ meesage: "fullName is required" });
-  if (!req.body.password || !req.body.confirmPassword)
-    return res
-      .status(400)
-      .json({ message: "password or confirmPassword is required" });
-  if (req.body.password !== req.body.confirmPassword)
-    return res.status(400).json({ message: "confirmPassword Not Match" });
-  if (!req.body.imageUrl)
-    return res.status(400).json({ message: "ImageUrl is required" });
-
-  // res.send('Register Success')
-  res.json({
-    message: "register success",
-    data: req.body,
-  });
-});
+router.post("/register", UserController.registerController);
+router.post("/login", UserController.loginController);
+router.get("/", UserController.getUserController);
 
 export default router;
