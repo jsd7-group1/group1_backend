cffb999ecc7e997dd81db73785a7f74cf4bb19c6
Kanya.wd <117735915+kanyawadee24@users.noreply.github.com>
2024-07-08 04:07:52 +0700

    register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,29 +1,79 @@
-import user from "../../models/user.model.js";
 import NotFoundError from "../error/NotFoundError.js";
+import User from "../../models/user.model.js";
+import { hashPassword } from "../utils/hash.js";
+import { sign } from "../utils/token.js";
 
 // Register Contrller
-const registerController = async (req, res) => {
-  console.log(req.body.email);
-  if (!req.body.email)
-    return res.status(400).json({ message: "email is required" });
-  if (!req.body.email.includes("@"))
-    return res.status(400).json({ message: "invalid email" });
-  if (!req.body.fullName)
-    return res.status(400).json({ meesage: "fullName is required" });
-  if (!req.body.password || !req.body.confirmPassword)
-    return res
-      .status(400)
-      .json({ message: "password or confirmPassword is required" });
-  if (req.body.password !== req.body.confirmPassword)
-    return res.status(400).json({ message: "confirmPassword Not Match" });
-  if (!req.body.imageUrl)
-    return res.status(400).json({ message: "ImageUrl is required" });
+const registerController = async (req, res, next) => {
+  try {
+    const {
+      email,
+      fullName,
+      password,
+      confirmPassword,
+      imageUrl,
+      companyID,
+      status,
+      userType,
+      createdByUserID,
+    } = req.body;
 
-  // res.send('Register Success')
-  res.json({
-    message: "register success",
-    data: req.body,
-  });
+    // Validation
+    if (!email) return res.status(400).json({ message: "email is required" });
+    if (!email.includes("@"))
+      return res.status(400).json({ message: "invalid email" });
+    if (!fullName)
+      return res.status(400).json({ meesage: "fullName is required" });
+    if (!password || !confirmPassword)
+      return res
+        .status(400)
+        .json({ message: "password or confirmPassword is required" });
+    if (password !== confirmPassword)
+      return res.status(400).json({ message: "confirmPassword Not Match" });
+    if (!imageUrl)
+      return res.status(400).json({ message: "ImageUrl is required" });
+
+    // 2.เช็ค user ซ้ำ (Mongoose)
+    const oldUser = await User.findOne({ email: req.body.email });
+    if (oldUser) {
+      return res.status(400).json({ message: "email already in use" });
+    }
+
+    // 3.Hashed
+    const hashedPassword = await hashPassword(password);
+
+    // 4.สร้าง User
+
+    const user = await User.create({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+      imageUrl: imageUrl,
+      createdByUserID: createdByUserID,
+      companyID: companyID,
+      status: status,
+      userType: userType,
+      isDelete: false,
+      isActive: true,
+    });
+
+    const returnedUser = {
+      id: user.id,
+      email: user.email,
+    };
+
+    // 5.ออก token
+    const accessToken = sign(returnedUser);
+
+    // 6.ส่งข้อมูลกลับ
+    res.json({
+      message: "register success",
+      data: returnedUser,
+      access_token: accessToken,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // Login
